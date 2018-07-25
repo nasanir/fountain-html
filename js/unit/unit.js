f062418ff60bcb64b283716a7f9eab2dd53242d2
nasanir <nasanir@163.com>
2018-07-25 18:07:43 +0800

    complete some things

--- js/unit/unit.js
+++ js/unit/unit.js
@@ -1,111 +1,152 @@
-function createDiv(var id,
-	var className) {
+function otherType(owner, context, styleType) {
+	if(context.length > 0) {
+		owner.text(context);
+	}
+
+	if(styleType.length > 0) {
+		owner.attr("style", styleType);
+	}
+}
+
+function createDiv(id,
+	className,
+	context, styleType) {
 	var div = $("<div></div>")
 		.attr("class", className)
 		.attr("id", id);
+
+	otherType(div, context, styleType);
 	return div;
 }
 
-function createH(var id,
-	var className,
-		var level) {
+function createH(id,
+	className,
+	level,
+	context,styleType) {
 	var h = $("<h" + level + "></h" + level + ">")
 		.attr("class", className)
 		.attr("id", id);
+
+	otherType(h, context, styleType);
+
 	return h;
 }
 
-function createFold(var id,
-	var className,
-		var collapse,
-			var dataParent,
-				var href,
-					var context) {
+function createFold(id,
+	className,
+	collapse,
+	dataParent,
+	href,
+	context,styleType) {
 	var a = $("<a></a>")
 		.attr("data-toggle", collapse)
 		.attr("data-parent", dataParent)
 		.attr("id", id)
 		.attr("href", href)
-		.text(context);
+		.attr("class", className);
+
+	otherType(a, context, styleType);
+
 	return a;
 }
 
-function createPanel(var parent,
-	var index,
-		var data) {
-	var title = data['title'];
+function createP(id,
+	className, context,styleType) {
+	var p = $("<p></p>")
+		.attr("class", className)
+		.attr("id", id);
+
+	otherType(p, context, styleType);
+	return p;
+}
+
+function createButton(id,
+	className,
+	type, context, styleType) {
+	var button = $("<button></button>")
+		.attr("class", className)
+		.attr("id", id)
+		.attr("type", type);
+
+	otherType(button, context, styleType);
+
+	return button;
+}
+
+function createSpan(id,
+	className,
+	context,styleType) {
+	var span = $("<span></span>")
+		.attr("class", className)
+		.attr("id", id);
+
+	otherType(span, context, styleType);
+	return span;
+}
+
+function createDefalutButton(parent, ver) {
 
 	if(parent != null) {
+		var div_row = createDiv(parent.attr('id') + "_div_row_" + ver, "row", "", "");
+
+		var div_add = createDiv(parent.attr('id') + "_div_row_add_" + ver, "col-sm-1", "", "");
+		var p_add = createP(parent.attr('id') + "_p_add_" + ver, "", "", "");
+		var button_add = createButton(parent.attr('id') + "_button_add_" + ver, "btn btn-default btn-sm", "button", "", "");
+		var span_add = createSpan(parent.attr('id') + "_span_add_" + ver, "glyphicon glyphicon-plus", " 添加", "");
+
+		var div_del = createDiv(parent.attr('id') + "_div_row_del_" + ver, "col-sm-1", "", "");
+		var p_del = createP(parent.attr('id') + "_p_del_" + ver, "", "", "");
+		var button_del = createButton(parent.attr('id') + "_button_del_" + ver, "btn btn-default btn-sm", "button", "", "color: rgb(224, 22, 6);");
+		var span_del = createSpan(parent.attr('id') + "_span_del_" + ver, "glyphicon glyphicon-minus", " 删除", "");
+
+		button_add.append(span_add);
+		p_add.append(button_add);
+		div_add.append(p_add);
+
+		button_del.append(span_del);
+		p_del.append(button_del);
+		div_del.append(p_del);
+
+		div_row.append(div_add);
+		div_row.append(div_del);
+
+		return div_row;
+	}
+
+}
+
+function createPanel(parentid,
+	index,
+	data) {
+	var title = data['title'];
+	var context = data['context'];
+	if(parentid.length > 0) {
 		var timestamp = Date.parse(new Date());
 		var ver = index + "_" + timestamp;
 
-		var div_panel = createDiv("panel panel-primary", "div_panel_" + ver);
+		var div_panel = createDiv("div_panel_" + ver, "panel panel-primary", "", "");
 
-		var div_panel_head = createDiv("panel-heading", "div_panel_head_" + ver);
+		var div_panel_head = createDiv("div_panel_head_" + ver, "panel-heading", "", "");
 
-		var div_panel_head_h3 = createH("div_panel_head_h3_" + ver, "panel-title", "3");
+		var h_panel_head = createH("panel_head_h3_" + ver, "panel-title", "3", title, "");
 
-		var div_panel_head_a = createFold("div_panel_head_a_" + ver, "", "collapse", parent.getAttribute("id"), "div_panel_collapse" + ver, title)
+		var a_panel_head = createFold("panel_head_a_" + ver, "panel-title", "collapse", parentid, "#div_panel_collapse_" + ver, "", "");
 
-		div_panel_head_h3.appendChild(div_panel_head_a);
-		div_panel_head.appendChild(div_panel_head_h3);
-		div_panel.appendChild(div_panel_head)
+		a_panel_head.append(h_panel_head);
+		div_panel_head.append(a_panel_head);
+		div_panel.append(div_panel_head)
 
-		var div_panel = $("<div></div>")
-			.attr("class", "panel panel-primary")
-			.attr("id", "div_panel_" + ver);
+		var div_collapse = createDiv("div_panel_collapse_" + ver, "panel-collapse collapse", "", "");
 
-		<
-		div id = "collapseTwo"
-		class = "panel-collapse collapse" >
-			<
-			div class = "panel-body " >
-			<
-			p >
-			<
-			button type = "button"
-		class = "btn btn-default btn-sm" >
-			<
-			span class = "glyphicon glyphicon-plus" > < /span>添加 < /
-		button > <
-			/p>
+		var div_panel_body = createDiv("div_panel_body_" + ver, "panel-body", "", "");
 
-			<
-			div class = "well well-lg" > 您好， 我在大的 Well 中！ < /div>
+		var div_panel_body_well = createDiv("div_panel_body_well" + ver, "well well-lg", context, "");
 
-			<
-			div class = "row" >
-			<
-			div class = "col-sm-4" >
-			<
-			div class = "panel panel-info" >
-			<
-			div class = "panel-heading" >
-			<
-			h3 class = "panel-title" > 面板标题 < /h3> < /
-		div > <
-			div class = "panel-body" >
-			这是一个基本的面板 <
-			/div> < /
-		div > <
-			/div> <
-		div class = "col-sm-4" >
-			<
-			div class = "panel panel-info" >
-			<
-			div class = "panel-heading" >
-			<
-			h3 class = "panel-title" > 面板标题 < /h3> < /
-		div > <
-			div class = "panel-body" >
-			这是一个基本的面板 <
-			/div> < /
-		div > <
-			/div> < /
-		div >
+		div_panel_body.append(createDefalutButton(div_panel_body, ver));
+		div_panel_body.append(div_panel_body_well);
+		div_collapse.append(div_panel_body);
+		div_panel.append(div_collapse);
 
-			<
-			/div>
-
+		return div_panel;
 	}
 }
